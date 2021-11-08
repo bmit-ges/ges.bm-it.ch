97f9ebba3531eda0709934c9e2c3b446fa1b9c7d
Gehring <sibylle.gehring@EDU.LAN>
2021-11-08 07:29:32 +0100

    reupload

---
+++ zertifikate.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css" />
+  <script src="javascript/script.js" defer></script>
+  <title>Zertifikate</title>
+</head>
+
+<body style="background-color:#000000;">
+
+
+  <div class="main-rec">
+      <header>
+        <nav id = menu class="menu">
+          <ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="about_me.html">About Me</a></li>
+            <li><a href="projekte.html">Projekte</a></li>
+            <li><a href="zertifikate.html">Zertifikate</a></li>
+            <li><a href="lebenslauf.html">Lebenslauf</a></li>
+            <li><a href="kontakt.php">Kontakt</a></li>
+          </ul>
+        </nav>
+        
+      </header>
+      <div class= "line"></div>
+ 
+  </div>
+
+  <div class ="rectangle-zert"></div>
+  <div id="zert-line"></div>
+
+  <!---- IMS F Zertifikat---->
+
+  <img id="zertifikat1" src="bilder/imsf.jpg" alt="imsf">
+
+  <div id=zert-titel1>
+    <p> IMS F Zertifikat</p>
+
+  </div>
+
+  <div id=zert-p1>
+    <p>
+      IMS F Zertifikat mit Schwerpunktsfächern Chemie,
+      <br>Biochemie und Eurythmie. Der IMS F Abschluss ist gleichwertig
+      <br>wie der Fachmittelschulabschluss und ermöglicht den Besuch
+      <br> einer Fachhochschule oder einer Höheren Fachschule.
+    </p>
+  </div>
+
+
+    <!---beobachtungsschwerpunkt-->
+    <img id="zertifikat2" src="bilder/beobachtungsschwerpunk.jpg" alt="beobachtungsschwerpunk">
+
+    <div id=zert-titel2>
+      <p> Beobachtungsschwerpunk </p>
+
+    </div>
+
+    <div id=zert-p2>
+      <p>
+        Zeugnis des Beobachtungsschwerpunktes aus dem zweiten
+        <br>Schuljahr der Atelierschule. Schwerpunktfach war Mathematik.
+        <br>Die geprüften Schwerpunkte lagen dabei auf Fachwissen
+        <br>(Anwendung und Formulierung), Verständnis in den
+        <br>verschiedenen Gebieten und Teamarbeit, hierbei
+        <br>verschiedenen Themen an andere Mitschüler*innen
+        <br>wie man die weitergebenkann und ob
+        <br>sie das Erklärte danach selbst anwenden können.
+
+      </p>
+    </div>
+    <!--Multicheck-->
+    <img id="zertifikat3" src="bilder/multicheck.jpg" alt="multicheck">
+
+    <div id=zert-titel3>
+      <p> Multicheck </p>
+
+    </div>
+
+    <div id=zert-p3>
+      <p>
+        Einstufungstest in der Kategorie Informatikerin
+        <br>EFZ Applikationsentwicklung.
+      </p>
+    </div>
+
+    <!--ECDL Basic-->
+    <img id="zertifikat4" src="bilder/coming-soon.jpg" alt="coming-soon">
+
+    <div id=zert-titel4>
+      <p> ECDL Basic </p>
+
+    </div>
+
+    <div id=zert-p4>
+      <p>
+        European Computer Driving Licence, anerkannter Computer Kurs.
+        <br>ECDL Basic Zertifikat beinhaltet den Abschluss in
+        <br>Computer-Grundlagen, Online-Grundlagen, Textverarbeitung
+        <br>und Tabellenkalkulation.
+        <br>
+        <br>
+        <br>Ich habe wie folgt in den einzelnen Modulen Abgeschlossen:
+        <br>Computer-Grundlagen: 94%
+        <br>Online-Grundlagen: 100%
+        <br>Textverarbeitung: 97%
+        <br>Tabellenkalkulation: 89%
+
+      </p>
+    </div>
+     <!--ECDL Standart-->
+
+    <img id="zertifikat5" src="bilder/coming-soon.jpg" alt="coming-soon">
+
+    <div id=zert-titel5>
+      <p> ECDL Standart </p>
+
+    </div>
+
+    <div id=zert-p5>
+      <p>
+        European Computer Driving Licence, anerkannter Computer Kurs.
+        <br>ECDL Standart Zertifikat beinhaltet das ECDL Basic Zertifikat plus 
+        <br>den Abschluss in IT-Sicherheit, Präsentation 
+        <br>und Online-Zusammenarbeit.
+        <br>
+        <br>
+        <br>Ich habe wie folgt in den einzelnen Modulen Abgeschlossen:
+        <br>IT-Sicherheit: 86%
+        <br>Präsentation: 94%
+        <br>Online-Zusammenarbeit: 100%
+      </p>
+    </div>
+
+    <!--Navigation-->
+
+    <img id="zert-arrow-right" src="bilder/arrow_right.png" alt="zert_arrow_right">
+    <img id="zert-arrow-left" src="bilder/arrow_left.png" alt="zert_arrow_left">
+  
+  </div>
+  
+  <footer>
+    <div class = "footer-text">Erstellt von Sibylle Gehring | HTML, CSS, JS</div>
+  </footer>
+</body>
+
+</html>
