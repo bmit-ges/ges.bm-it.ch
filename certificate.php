0e2afef83729aeb1dab5c09173f5d2f8d3debc48
Gehring <sibylle.gehring@EDU.LAN>
2023-03-31 10:27:43 +0200

    certificates

--- certificate.php
+++ certificate.php
@@ -32,12 +32,11 @@
     <link rel="stylesheet" href="css/smalscreen.css">
     <link rel="stylesheet" href="css/certificate.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
+    <script src="/js/slideshow.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-    
 </head>
 
-<body>
+<body onload="slideshow()">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
@@ -66,43 +65,12 @@
         <img class="slide" style="width: auto; height: 700px;" src="img/multicheck.jpg">
     </div>
     <div class="button-container">
-        
         <button class="btn btn-outline-light" onclick="plusDivs(-1)">❮ Prev</button>
         <button class="btn btn-outline-light" onclick="currentDiv(1)">1</button> 
         <button class="btn btn-outline-light" onclick="currentDiv(2)">2</button> 
         <button class="btn btn-outline-light" onclick="currentDiv(3)">3</button> 
         <button class="btn btn-outline-light" onclick="plusDivs(1)">Next ❯</button>
     </div>
-
-    <script>
-   
-        var slideIndex = 1;
-        showDivs(slideIndex);
-
-        function plusDivs(n) {
-        showDivs(slideIndex += n);
-        }
-
-        function currentDiv(n) {
-        showDivs(slideIndex = n);
-        }
-
-        function showDivs(n) {
-        var i;
-        var x = document.getElementsByClassName("slide");
-        var dots = document.getElementsByClassName("no");
-        if (n > x.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = x.length}
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";  
-        }
-
-        x[slideIndex-1].style.display = "block";  
-  
-        }
-    </script>
-
-
     <footer>
         <div class="footer-container">
             <p class="footer-text"> Erstellt von Sibylle Gehring | HTML, CSS, JS, JQuery, PHP, Bootstrap</p>
